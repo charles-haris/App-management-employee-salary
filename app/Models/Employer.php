b570c03d1b6ca7eaee915a41462491632b853aa9
BOUASSE BU KOMBILE Charles-Haris <52077878+charles-haris@users.noreply.github.com>
2024-11-01 17:09:20 +0000

    Update Employer.php

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -23,6 +23,7 @@ class Employer extends Model
     //what is the use of this method
     //what is the use of this method
     //what is the use of this method
+    //what is the use of this method
 
 
 
